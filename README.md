12bd1fc775ecf505f19d30dea15cc44344ed23da
Patryk Karwowski <p-karwowski-dev@gmail.com>
2025-02-06 17:06:44 +0000

    Documentation

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
-# User Authentication for project based on React Next Redux Toolkit TypeScript
+# Simple Authentication for SPAs using Http-Only cookies
 
-## How to run application:
+### Pros:
+
+- Tokens stored in secured fashion
+- Scalability from stateless auth - no session store
+- No token management on Frontend
+- Prevention from CSF attacks
+- Optionally prevention from CSF attacks
+
+### How does it work?
+
+On user login short and long session JWT tokens are created and insert
+to the corresponding cookies. Server defines to which upcoming requests
+attach the cookies. Short-live cookie should be attached to all
+protected routs, and long-live cookie to only 'refresh' route.
+Http-Only cookies are managed and accessible only by server. This
+eliminates exposing cookies content to the client and risk of XSS
+attacks. Cookies can't be modified by client so there is no need for
+creating session store in database.
+
+# Example with Next and Redux (RTK Query):
+
+On server side sessions are managed in routs and verify in middleware. Client catches auth errors in custom fetchBaseQuery from RTK query. This can be done in the same manner with Axios' interceptors.
+
+### Handling short session on the server side.
+
+Middleware verification checks if short session cookie exists and if contains valid JWT token. When verification fails new header is attached to request. This flag is caught in the endpoint and server sends back 401 response.
+
+### Handling 401 unauthenticated request on the client side.
+
+When 401 responses is caught in custom fetchBaseQuery client is making secondary call to 'refresh' endpoint. When response is successful initial request is made again. Otherwise user is redirected to not restricted page.
+
+### Handling long session (refresh) on the server side.
+
+'Refresh' endpoint verifies long session JWT token extracted from long-live cookie. If verification is successful server creates new JWT tokens for both sessions, inserts them to new created corresponding cookies, and sends 200 response. Otherwise 401 respond is send.
+
+# Installation:
+
+The app requires creating .env.local file with a mongoDB URL to your database of users and a secret key for encrypting/decrypting JWT tokens.
+
+```
+.env.local
+MONGODB_URL=
+USER_SESSION_KEY=
+```
 
 ```bash
 npm install
@@ -8,12 +51,4 @@ npm install
 
 ```bash
 npm run dev
-```
-
-## Steps
-
-- 1. Boilerplate installation and folder cleanup
-
-```bash
-npx create-next-app --example with-redux with-redux-app
 ```
--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,81 @@
 export default function IndexPage() {
   return (
     <section>
-      <h1>
-        Secured stateless authentication with HttpOnly cookie for applications
-        build with Next, React and Redux.
-      </h1>
+      <h1>Simple Authentication for SPAs using Http-Only cookies</h1>
 
       <p>
-        Short session is used to give user access to restricted content while
-        long session extends user login time and refreshes short session.
-        Logging out user closes both short and long session, and user have to
-        login again.
+        <b>Pros:</b>
       </p>
+
+      <li> Tokens stored in secured fashion</li>
+      <li> Scalability from stateless auth - no session store</li>
+      <li> No token management on Frontend</li>
+      <li> Prevention from CSF attacks</li>
+      <li> Optionally prevention from CSF attacks</li>
 
       <p>
         <b>How does it work?</b>
       </p>
       <p>
-        HttpOnly cookies are accessible only by server which eliminates risk of
-        exposing cookies to XSS attacks on the client side. Server defines to
-        which requests cookie will be attached by setting path. When user logs
-        in to the app short and long session jwt tokens are created and inserted
-        to the corresponding cookies. Then short-live cookie is attached to all
-        requests sent to protected routs, and long-live cookie to '/refresh'
-        route only to increase security.
+        On user login short and long session JWT tokens are created and insert
+        to the corresponding cookies. Server defines to which upcoming requests
+        attach the cookies. Short-live cookie should be attached to all
+        protected routs, and long-live cookie to only 'refresh' route. Http-Only
+        cookies are managed and accessible only by server. This eliminates
+        exposing cookies content to the client and risk of XSS attacks. Cookies
+        can't be modified by client so there is no need for creating session
+        store in database.
+      </p>
+
+      <h1>Auth with Next.js and Redux (RTK Query) library</h1>
+
+      <p>
+        On server side sessions are managed in routs and verify in middleware.
+        Client catches auth errors in custom fetchBaseQuery from RTK query. This
+        can be done in the similar manner for any UI with Axios' interceptors.
       </p>
 
       <p>
-        <b>Handing short session on the server side.</b>
+        <b>Handling short session on the server side.</b>
       </p>
       <p>
         Middleware verification checks if short session cookie exists and if
-        contains valid jwt token. When verification fails a flag (new header) is
-        attached to request. This flag is caught in the endpoint and server
-        sends back 401 response.
+        contains valid JWT token. When verification fails new header is attached
+        to request. This flag is caught in the endpoint and server sends back
+        401 response.
       </p>
 
       <p>
-        <b>Handing 401 unauthenticated request on the client side.</b>
+        <b>Handling 401 unauthenticated request on the client side.</b>
       </p>
       <p>
         When 401 responses is caught in custom fetchBaseQuery client is making
-        secondary call to '/refresh' endpoint. When response is successful
+        secondary call to 'refresh' endpoint. When response is successful
         initial request is made again. Otherwise user is redirected to not
         restricted page.
       </p>
 
       <p>
-        <b>Handing long session (refresh) on the server side.</b>
+        <b>Handling long session (refresh) on the server side.</b>
       </p>
       <p>
-        'Refresh' endpoint verifies long session jwt token extracted from
-        long-live cookie. If verification is successful server creates new jwt
+        'Refresh' endpoint verifies long session JWT token extracted from
+        long-live cookie. If verification is successful server creates new JWT
         tokens for both sessions, inserts them to new created corresponding
         cookies, and sends 200 response. Otherwise 401 respond is send.
       </p>
 
       <p>
-        <b>Steps to test authentication.</b>
+        <b>Steps to use this Demo:</b>
       </p>
       <ol>
-        <li>Login to the account (login: Joe, password: pass).</li>
-        <li>Open 'Restricted' page.</li>
-        <li>Open network tab in developer tool.</li>
-        <li>Click a button to expire authentication token.</li>
-        <li>Observe sent and received requests.</li>
-        <li>Logout from the app.</li>
+        <li>Login to the account (login: Joe, password: pass)</li>
+        <li>Open 'Restricted' page</li>
+        <li>Open network tab in developer tool</li>
+        <li>Click a button to expire authentication token</li>
+        <li>Observe sent and received requests</li>
+        <li>Logout from the app</li>
+        <li>Open 'Restricted' page again</li>
       </ol>
     </section>
   )
